496c096fd6357a871396da7c600dede6e93f1e86
user <user>
2019-07-26 15:11:42 +0800

    version1

--- readme.js
+++ readme.js
@@ -1,20 +1,5 @@
-/*
-   第一步，用命令git add 告诉git,把文件放到git 仓库
-   git add readme.js 
-
-   第二步，用命令git commit 告诉git,把文件提交到仓库
-   git commit -m "wrote a readme file"
-
-   小结：
-        初始化一个git 仓库 git  init
-        添加文件到 git 仓库，分2步
-
-        添加文件到git 仓库，分为两步
-        1.git add <file>,注意，可反复多次使用，添加多个文件
-        2.使用命令 git commit -m <message>,完成
-
-*/
 
 export default  {
-    lal :'333'
+    lal :'333',
+    version:'222'
 }
